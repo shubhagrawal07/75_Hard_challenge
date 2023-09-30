0f50e299ea68b79cfdc5769c2d10d7bbd483bed6
Shubh Agrawal <shubhagrawal0710@gmail.com>
2023-09-30 22:15:38 +0530

    Palindrome partitioning added

--- Minimun_sum_partiotion.cpp
+++ Minimun_sum_partiotion.cpp
@@ -26,27 +26,31 @@ int minDifference(int arr[], int n)  {
 	    
 	    for(int i=1;i<n+1;i++){
 	        for(int j=1;j<half+1;j++){
+
+				// if current element in the array is smaller than or equal to the half sum, we have two choices,
+				// either take or not take. we will go with the one which makes our sum maximum.
 	            if(arr[i-1]<=j) dp[i][j] = max(dp[i-1][j],arr[i-1] + dp[i-1][j-arr[i-1]]);
+
+				// if the element is greater than the half sum, we have no choice except not to take it.
 	            else dp[i][j] = dp[i-1][j];
 	        }
 	    }
 
-		// returning the difference of the two subset sums
+		// the final value stored in dp at index (n,half)
+		// is the maximum sum of a subset smaller than or equal to half sum
+		// this is one of the subset we need, other can be obtained by subtracting it by total sum
 	    
+		// final ans
 	    return abs(sum-2*dp[n][half]);
 	}
 
 int32_t main() {
 
 	
-	int t;
-	cin>>t;
-	while(t--){
-	    int n=4;
-		int arr[n] = {1,6,11,5};
+	int n=4;
+	int arr[n] = {1,6,11,5};
 	    
-	    cout<<minDifference(arr,n)<<endl;
-	}
+	cout<<minDifference(arr,n)<<endl;
 	
 	return 0;
 }
---
+++ Palindrome_partitioning.cpp
@@ -0,0 +1,75 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Problem statement:
+// Given a string str, a partitioning of the string is a palindrome partitioning
+// if every sub-string of the partition is a palindrome.
+// Determine the fewest cuts needed for palindrome partitioning of the given string.
+
+// Topics: DP, recursion, MCM
+
+// Time complexity: O(N2)
+// Space complexity: O(N2)
+
+// Test case: "ababbbabbababa"
+// output: 3
+// eg: "a", "babbbab", "b", "ababa".
+
+
+int dp[501][501];
+
+    int isPal(string s,int i,int j){
+        while(i<=j){
+            if(s[i]!=s[j]) return 0;
+            i++;
+            j--;
+        }
+        return 1;
+    }
+
+    int solve(string str,int i,int j){
+        //base case
+        if(i>j) return 0;
+        
+        // if string is already a palindrome, no need to split , hence ans=0
+        if(isPal(str,i,j)) return dp[i][j] = 0;
+        
+        // if we already calculated for this input, just use the previous ans
+        if(dp[i][j]!=-1) return dp[i][j];
+        
+        
+        int ans=INT_MAX;
+        
+        //taking a variable k and spliting the curr string in 2 string (i,k) and (k+1,j)
+        for(int k=i;k<j;k++){
+            
+            // using recursion to solve for the two splited strings
+            if(dp[i][k]==-1) dp[i][k]=solve(str,i,k);
+            if(dp[k+1][j]==-1) dp[k+1][j] = solve(str,k+1,j);
+            
+            //storing the ans in the temp variable for the current slipt
+            int temp = 1+dp[i][k]+dp[k+1][j];
+            
+            // checking if the current no. splits is smaller than the previos splits
+            ans=min(ans,temp);
+        }
+        return dp[i][j] = ans;
+    }
+
+    int palindromicPartition(string str)
+    {
+        int n=str.length();
+        
+        // initializing the dp by -1
+        memset(dp,-1,sizeof(dp));
+        
+        return solve(str,0,n-1);
+    }
+
+int32_t main() {
+
+	string s = "ababbbabbababa";
+	cout<<palindromicPartition(s)<<endl;
+	
+	return 0;
+}
